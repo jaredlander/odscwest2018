c67b2e5a305029e9f0e8c9a369f3ca8fcb9dfa12
Jared Lander <info@jaredlander.com>
2018-10-31 17:51:38 -0400

    Started xgboost

---
+++ code/xgboost.r
@@ -0,0 +1,20 @@
+library(useful)
+library(magrittr)
+library(dygraphs)
+library(xgboost)
+
+land_train <- readr::read_csv('data/manhattan_Train.csv')
+land_val <- readRDS('data/manhattan_Validate.rds')
+
+table(land_train$HistoricDistrict)
+
+histFormula <- HistoricDistrict ~ FireService + 
+    ZoneDist1 + ZoneDist2 + Class + LandUse + 
+    OwnerType + LotArea + BldgArea + ComArea + 
+    ResArea + OfficeArea + RetailArea + 
+    GarageArea + FactryArea + NumBldgs + 
+    NumFloors + UnitsRes + UnitsTotal + 
+    LotFront + LotDepth + BldgFront + 
+    BldgDepth + LotType + Landmark + BuiltFAR +
+    Built + TotalValue - 1
+
