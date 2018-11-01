191af2d9a6dfab6b19c463fd78f9ac02bf72e108
Jared Lander <info@jaredlander.com>
2018-11-01 18:03:02 -0400

    Built out plots panel

--- app/ui.r
+++ app/ui.r
@@ -1,7 +1,33 @@
 library(shiny)
 
+plotsPanel <- tabPanel(
+    title='Plots',
+    fluidRow(
+        column(
+            width=4,
+            selectInput(
+                inputId='ColumnName',
+                label='Choose a column',
+                choices=names(mtcars)
+            ),
+            sliderInput(
+                inputId='NumBins',
+                label='Choose the number of bins',
+                min=5, max=50, value=25
+            )
+        ),
+        column(
+            width=8,
+            plotOutput(
+                outputId='CarHistogram'
+            )
+        )
+    )
+)
+
 navbarPage(
     title='My First Shiny App',
+    selected='Plots',
     tabPanel(
         title='Legend of Zelda',
         'An adventure to save the world from an evil warmonger'
@@ -9,5 +35,6 @@ navbarPage(
     tabPanel(
         title='A Link to the Past',
         'It happened again'
-    )
+    ),
+    plotsPanel
 )
