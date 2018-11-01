597c04e585d49f3216387b8ffdfd0d9c19724dbe
Jared Lander <info@jaredlander.com>
2018-11-01 17:30:02 -0400

    Started server and ui

---
+++ app/ui.r
@@ -0,0 +1,13 @@
+library(shiny)
+
+navbarPage(
+    title='My First Shiny App',
+    tabPanel(
+        title='Legend of Zelda',
+        'An adventure to save the world from an evil warmonger'
+    ),
+    tabPanel(
+        title='A Link to the Past',
+        'It happened again'
+    )
+)
