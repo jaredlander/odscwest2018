bf8a0175d7c9ce1216ba6d0f927fe2109c5d46d0
Jared Lander <info@jaredlander.com>
2018-11-01 18:11:34 -0400

    Added plot to server

--- app/server.r
+++ app/server.r
@@ -1,5 +1,11 @@
 library(shiny)
+library(ggplot2)
 
 shinyServer(function(input, output, session){
+    
+    output$CarHistogram <- renderPlot(
+        ggplot(mtcars, aes_string(x=input$ColumnName)) + 
+            geom_histogram(bins=input$NumBins)
+    )
     
 })
